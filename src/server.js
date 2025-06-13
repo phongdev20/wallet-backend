d4b9812a59e6bf90bbb4ba1ebd6a1efc3489c917
tranphong <phongdevinfo@gmail.com>
2025-06-14 01:23:42 +0700

    update cron

--- src/server.js
+++ src/server.js
@@ -3,9 +3,12 @@ import express from "express";
 import { initDB, sql } from "./config/db.js";
 import ratelimiter from "./middleware/rateLimiter.js";
 import transactionRouter from "./routers/transactionsRoute.js";
+import job from "./config/cron.js";
 
 configDotenv();
 const app = express();
+
+if (process.env.NODE_ENV === "production") job.start;
 
 // middleware
 app.use(ratelimiter);
@@ -13,8 +16,10 @@ app.use(express.json());
 
 const PORT = process.env.PORT;
 
-app.get("/", (req, res) => {
-  res.send("It's working");
+app.get("/api/health", (req, res) => {
+  res.status(200).json({
+    status: "ok",
+  });
 });
 
 app.use("/api/transactions", transactionRouter);
